510790077ecc71f3955e68f0683fe238c22269c5
Mattheo de Wit <mattheodw@gmail.com>
2020-12-07 08:05:05 +0100

    Update PartOne.java

--- day7/PartOne.java
+++ day7/PartOne.java
@@ -16,7 +16,6 @@ public class PartOne {
             if (l.contains(a) && !l.startsWith(a)) {
                 String[] b = l.split(" ");
                 String valid = b[0] + " " + b[1];
-                System.out.println(valid);
                 if (!result.contains(valid)) {
                     result.add(valid);
                     method(valid);
