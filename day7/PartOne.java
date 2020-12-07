27c846b5608243d291068368ef6eea4d779a33a7
Mattheo de Wit <mattheodw@gmail.com>
2020-12-07 07:39:41 +0100

    Create PartOne.java

---
+++ day7/PartOne.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static List<String> result = new ArrayList<>();
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner s = new Scanner(new File("input1.txt"));
+        method("shiny gold");
+        System.out.println(result.size());
+    }
+
+    public static void method(String a) throws FileNotFoundException {
+        Scanner s = new Scanner(new File("input1.txt"));
+        while (s.hasNextLine()) {
+            String l = s.nextLine();
+            if (l.contains(a) && !l.startsWith(a)) {
+                String[] b = l.split(" ");
+                String valid = b[0] + " " + b[1];
+                System.out.println(valid);
+                if (!result.contains(valid)) {
+                    result.add(valid);
+                    method(valid);
+                }
+            }
+        }
+    }
+}
