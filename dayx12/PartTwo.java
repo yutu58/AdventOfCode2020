cf07776bdc167665e1976bae66f1c3af342a6b5b
Mattheo de Wit <mattheodw@gmail.com>
2020-12-12 11:10:50 +0100

    Create PartTwo.java

---
+++ dayx12/PartTwo.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner s = new Scanner(new File("input1.txt"));
+        int x = 0;
+        int y = 0;
+        int d = 0;
+
+        int xw = 10;
+        int yw = 1;
+
+        while (s.hasNextLine()) {
+            String l = s.nextLine();
+            char a = l.charAt(0);
+            int b = Integer.parseInt(l.substring(1));
+            switch(a){
+                case 'N':
+                    yw = yw + b;
+                    break;
+                case 'E':
+                    xw = xw + b;
+                    break;
+                case 'S':
+                    yw = yw - b;
+                    break;
+                case 'W':
+                    xw = xw - b;
+                    break;
+                case 'L':
+                    for (int i = 0; i < (b / 90); i++){
+                        int oldxw = xw;
+                        xw = -yw;
+                        yw = oldxw;
+                    }
+                    break;
+                case 'R':
+                    for (int i = 0; i < (b / 90); i++) {
+                        int oldxw = xw;
+                        xw = yw;
+                        yw = -oldxw;
+                    }
+                    break;
+                case 'F':
+                    for (int i = 0; i < b; i++){
+                        x = x + xw;
+                        y = y + yw;
+                    }
+                    break;
+            }
+        }
+        System.out.println(Math.abs(x) + Math.abs(y));
+    }
+}
